c5c467b756b7eb65637a7987e14fdb8a01a623dd
unJaco <73906102+unJaco@users.noreply.github.com>
2022-07-12 16:56:49 +0200

    test api call

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,37 @@ console.log('Script started successfully');
 
 let currentPopup: any = undefined;
 
+let obj: any = undefined;
+
 // Waiting for the API to be ready
 WA.onInit().then(() => {
+
     console.log('Scripting API ready');
     console.log('Player tags: ',WA.player.tags)
 
     WA.room.onEnterLayer('clockZone').subscribe(() => {
+
+        const userAction = async () => {
+            const response = await fetch('http://localhost:8080/api/v1/book');
+            const myJson = await response.json(); //extract JSON from the http response
+            // do something with myJson
+
+           obj = JSON.parse(myJson);
+
+          }
+
+
+
         const today = new Date();
-        const time = today.getHours() + ":" + today.getMinutes();
-        currentPopup = WA.ui.openPopup("clockPopup","It's " + time,[]);
+        const time = today.getHours() + ":" + today.getMinutes();   
+
+        if(obj){
+            currentPopup = WA.ui.openPopup("clockPopup","It's " + obj.title,[]);
+        } else {
+            currentPopup = WA.ui.openPopup("clockPopup","It's " +time,[]);
+        }
+        
+        
     })
 
     WA.room.onLeaveLayer('clockZone').subscribe(closePopUp)
